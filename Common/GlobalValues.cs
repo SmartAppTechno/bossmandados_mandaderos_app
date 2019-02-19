60afce84aae49edd49fe45a0672ca3df2395ad54
carolinadp <caroagsmex@hotmail.com>
2019-02-18 18:10:04 -0600

    Fix de global address
    Fix en la global address cambiar local de testing por global.

--- Common/GlobalValues.cs
+++ Common/GlobalValues.cs
@@ -6,9 +6,9 @@ namespace Common
         //public const string AppURL = @"https://bossmandadosapi.azurewebsites.net";
 
         //TODO: QUITAR EL LOCALHOST
-        public const string AppURL = @"http://localhost:50013/";
+        //public const string AppURL = @"http://localhost:50013/";
 
-        //public const string AppURL = @"https://backend.bossmandados.com";
+        public const string AppURL = @"https://backend.bossmandados.com";
         public const string ListenConnectionString = "Endpoint=sb://bossmandadosnotification.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=YdrSt4M5gVAJuzxzpN+CJRTA9AiYK2dZpmrNiBZ2DbQ=";
         public const string NotificationHubName = "BossMandados_Notification";
     }
